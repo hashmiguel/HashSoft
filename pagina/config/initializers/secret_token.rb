18b1fd46f1de9a97259b662a2ffb888d51c61ee1
Miguel Z <poserex@gmail.com>
2014-06-10 15:02:14 -0500

    "iniciando Proyecto

---
+++ pagina/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pagina::Application.config.secret_key_base = 'c4d011c789dd6708b990e5a51055d17a376e0c9d96c1140dd2f0b6aa568d760230d9c4e1c3fae85e7513547f1efce500f44004700cde2af6eda1d4648404ec29'
