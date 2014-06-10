f64ebcc4c93015b4f6c081dc2ceb37b12016c4eb
Miguel Z <poserex@gmail.com>
2014-06-10 16:34:46 -0500

    "Se genero el controlador de las paginas"

--- pagina/config/routes.rb
+++ pagina/config/routes.rb
@@ -1,4 +1,5 @@
 Pagina::Application.routes.draw do
+  get "paginas/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
